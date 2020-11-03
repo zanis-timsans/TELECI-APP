76471c75210e8452edd7b3a20a663c96ecd7225c
Žanis <zanis.timsans@gmail.com>
2020-11-03 16:21:12 +0200

    initial launch

--- app.py
+++ app.py
@@ -24,75 +24,21 @@ server = app.server
 
 # PREPROCESSING Load dataframe
 camera = dict(
-    eye=dict(x=2, y=2, z=0.8)
-)
-
-# Create dataframe for visualising 'Complete learning acquisition landscape'
-df_tele = pd.DataFrame(data=[['too complicated content', 0.222, 0.111, 0.666],
-                             ['too easy content', 0, 1, 0, ],
-                             ['ideally matching content', 0.667, 0.333, 0]],
-                       columns=['content', 'N-P', 'P-P', 'X-N'], index=None)
-
-fig = go.Figure(data=[
-    go.Mesh3d(
-        x=df_tele['N-P'],
-        y=df_tele['P-P'],
-        z=df_tele['X-N'],
-        color='steelblue',
-        opacity=0.3,
-        hoverinfo='skip',
+        eye=dict(x=2, y=2, z=0.8)
     )
-])
 
-fig.update_layout(
-    template='plotly',
-    scene_camera=camera,
-    scene=dict(
-        xaxis_title="Matching",
-        yaxis_title="Easy",
-        zaxis_title="Complicated",
-        xaxis=dict(
-            nticks=6, range=[0, 1],
-            backgroundcolor="rgb(200, 200, 230)",
-            gridcolor="white",
-            showbackground=True,
-            zerolinecolor="white",
-            showspikes=False),
-        yaxis=dict(
-            nticks=6, range=[0, 1],
-            backgroundcolor="rgb(230, 200,230)",
-            gridcolor="white",
-            showbackground=True,
-            zerolinecolor="white",
-            showspikes=False),
-        zaxis=dict(
-            nticks=6, range=[0, 1],
-            backgroundcolor="rgb(230, 230,200)",
-            gridcolor="white",
-            showbackground=True,
-            zerolinecolor="white",
-            showspikes=False),
-    ),
-    height=600,
-    margin=dict(
-        r=0, l=0,
-        b=0, t=0),
-)
 
 # APPLICATION LAYOUT
 app.layout = html.Div([
     dbc.Row(dbc.Col([
-        html.H1('TELECI PROTOTYPE DATA VISUALIZATION', className="text-center my-5"),
-        html.Div(['''
-                        Welcome to data visualization web application for TELECI project. 
-                        Visualization method is called telecides and it reflects quantitative visualization of the 
-                        appropriateness of an e-content unit for the needs of 
-                        the specific learner or learners target group.
-                        ''',
-                  dcc.Link(html.H6('Go to course content', className='mt-3'), href='https://bpome.mii.lv/',
-                           target='_blank'),
-                  ], className="text-center my-5"),
-    ], width=7,
+        html.H1('TELECI DATA VISUALIZATION', className="text-center my-5"),
+        html.Div('''
+                Welcome to data visualization web application for TELECI project. 
+                Visualization method is called telecides and it reflects quantitative visualization of the 
+                appropriateness of an e-content unit for the needs of 
+                the specific learner or learners target group.
+                ''', className="text-center my-5"),
+    ], width=8,
     ), justify='center',
     ),
 
@@ -112,7 +58,8 @@ app.layout = html.Div([
             dcc.Dropdown(options=[
                 {'label': 'Business Planning for Open Markets and Entrepreneurship',
                  'value': 'https://bpome.mii.lv/webservice/rest/server.php?wstoken=abc77eef5247b3aa21488df6a394a3d4'
-                          '&wsfunction=local_getstudentactivitydata_functiongetstudentactivitydata&date_from=0'},
+                          '&wsfunction=local_getstudentactivitydata_functiongetstudentactivitydata&date_from=0'
+                          '=local_notifyemailsignup_functiongetstudentactivitydata&moodlewsrestformat=json'},
                 {'label': 'Other course',
                  'value': 'https://artss.mii.lv/webservice/rest/server.php?courseid=12&wstoken'
                           '=a78e76c2570f41a3f180d0979914c7dc&wsfunction'
@@ -120,7 +67,8 @@ app.layout = html.Div([
                  'disabled': True},
             ],
                 value='https://bpome.mii.lv/webservice/rest/server.php?wstoken=abc77eef5247b3aa21488df6a394a3d4'
-                      '&wsfunction=local_getstudentactivitydata_functiongetstudentactivitydata&date_from=0',
+                      '&wsfunction=local_getstudentactivitydata_functiongetstudentactivitydata&date_from=0'
+                      '=local_notifyemailsignup_functiongetstudentactivitydata&moodlewsrestformat=json',
                 searchable=True,
                 placeholder='Izvēlies vai ievadi kursa nosaukumu',
                 persistence=True,
@@ -132,11 +80,11 @@ app.layout = html.Div([
             html.Div(
                 [
                     html.Div([
-                        html.H6('Content suitability areas'),
-                        html.Img(title='example', src='assets/landscape-segments.jpg', width='100%'),
-                        dbc.Alert("Ideally matching content", color="success", className='mb-0'),
-                        dbc.Alert("Too complicated content", color="danger", className='my-1'),
-                        dbc.Alert("Too easy content ", color="primary", className='mt-0'),
+                        html.H6('Satura piemērotības zonas'),
+                        html.Img(title='paraugs', src='assets/landscape-segments.jpg', width='100%'),
+                        dbc.Alert("Atbilstošs saturs", color="success", className='mb-0'),
+                        dbc.Alert("Neatbilstošs saturs. Pārāk sarežģīts", color="danger", className='my-1'),
+                        dbc.Alert("Daļēji neatbilstošs, pārāk viegls saturs. ", color="primary", className='mt-0'),
                     ], className='mb-3 border p-3'),
                 ], className='ml-3'
             )
@@ -145,7 +93,7 @@ app.layout = html.Div([
             dbc.CardGroup([
                 dbc.Card(
                     dbc.CardBody([
-                        html.Div('Units', className="card-title text-center"),
+                        html.Div('Unit', className="card-title text-center"),
                         html.H1(id='tema', className='text-center clearfix')
                     ]), className=""
                 ),
@@ -157,7 +105,7 @@ app.layout = html.Div([
                 ),
                 dbc.Card(
                     dbc.CardBody([
-                        html.Div('Sub-units', className="card-title text-center"),
+                        html.Div('Question pairs', className="card-title text-center"),
                         html.H1(id='pari', className='text-center')
                     ]), className=""
                 ),
@@ -167,36 +115,19 @@ app.layout = html.Div([
                         html.H1(id='summa', className='text-center clearfix')
                     ]), className=""
                 )
-            ], className='mt-3 mb-3', style={'columnCount': 2}),
+            ], className='mt-3 mb-3'),
             dbc.Col([
+                html.H5('Tēmas', className='ac'),
                 dcc.Loading(id="loading-2", children=[
                     html.Div(id="loading-output-2", style={'display': 'none'}),
-                    dcc.Tabs([
-                        dcc.Tab(label='Units', children=[
-                            dcc.Graph(
-                                id='telecides-unit',
-                                figure=fig,
-                                config={'displaylogo': False, 'showTips': True},
-                            )
-                        ]),
-                        dcc.Tab(label='Sub-units', children=[
-                            dcc.Graph(
-                                id='telecides-sub-unit',
-                                figure=fig,
-                                config={'displaylogo': False, 'showTips': True},
-                            )
-                        ]),
-                        dcc.Tab(label='Students', children=[
-                            dcc.Graph(
-                                id='telecides-student',
-                                figure=fig,
-                                config={'displaylogo': False, 'showTips': True},
-                            )
-                        ]),
-                    ]),
+                    dcc.Graph(
+                        id='telecides-unit',
+                        figure={},
+                        config={'displaylogo': False, 'showTips': True},
+                    ),
                 ], type="default"),
-            ], className='border p-3'),
-        ], width=8),
+            ], className=''),
+        ], width=9),
     ], justify="center"),
 ])
 
@@ -204,8 +135,6 @@ app.layout = html.Div([
 # ------------------- CALLBACKS -------------------------------------------------------------------------------------
 @app.callback(
     [Output(component_id='telecides-unit', component_property='figure'),
-     Output(component_id='telecides-sub-unit', component_property='figure'),
-     Output(component_id='telecides-student', component_property='figure'),
      Output(component_id='tema', component_property='children'),
      Output(component_id='number_of_students', component_property='children'),
      Output(component_id='pari', component_property='children'),
@@ -270,144 +199,125 @@ def update_telecides(value):
     # Convert everything to uppercase in case there are some in lowercase
     rez['letter'] = rez['letter'].str.upper()
 
+    # All necessary 'A'
     a = rez[(rez['letter'].shift(-1) == 'B')].copy()
+
+    # All necessary 'B'
     b = rez[(rez['letter'] == 'B')].copy()
-    ab = a.append(b, sort=True)
-    ab.sort_values(by=['section', 'lessonid', 'user_id', 'letter'], inplace=True)
-    ab.reset_index(inplace=True)
-    ab.drop_duplicates(inplace=True)
-    a = ab[(ab['letter'] == 'A')].copy()
-    b = ab[(ab['letter'].shift(1) == 'A')].copy()
-    ab = a.append(b, sort=True)
-    ab.sort_values(by=['section', 'lessonid', 'user_id', 'letter'], inplace=True)
-    ab.reset_index(inplace=True)
-    ab.drop_duplicates(inplace=True)
+
+    # Combine two filtered dataframes
+    rez = a.append(b, sort=True)
+
+    # Sort values and reset index
+    rez.sort_values(by=['section', 'lessonid', 'user_id'], inplace=True)
+    rez.reset_index(inplace=True)
 
     # Number of question pairs
-    pari = ab["lessonid"].nunique()
+    pari = rez["lessonid"].nunique()
 
     # Number of students only
-    number_of_students = ab["user_id"].nunique()
+    number_of_students = rez["user_id"].nunique()
 
-    def final_aggregation(coll, x, y):
-        global temas
-        global krasa
-        global summa
-        if coll == 'lessonid':
-            content_filter = x['type'].isin(['content'])
-            content_df = x[['section', 'lessonid', 'title', 'type']]
-            content_df = content_df[content_filter].drop_duplicates(subset=['lessonid']).drop(['type'], axis=1).copy()
-            # create dictionary of lesson titles with corresponding lessonid
-            lesson_di = pd.Series(content_df.title.values, index=content_df.lessonid).to_dict()
-            krasa = 'dodgerblue'
+    # Join 'p' and 'n' results into one column based on 'a' and 'b' questions. Keep section number. Convert to dataframe
+    # using to_frame
+    final_df = rez.groupby(by=[rez.index // 2, 'section'])['correct_answer'].agg('-'.join).to_frame()
 
-        if coll == 'user_id':
-            krasa = 'lightcoral'
-        if coll == 'section':
-            krasa = 'mediumslateblue'
-        # Join 'p' and 'n' results into one column based on 'a' and 'b' questions. Keep lessonid number. Convert to
-        # dataframe using to_frame
-        x_df = y.groupby(by=[y.index // 2, coll])['correct_answer'].agg('-'.join).to_frame()
-        # Reset index
-        x_df.reset_index(level=[coll], inplace=True)
+    # Reset index
+    final_df.reset_index(level=['section'], inplace=True)
 
-        if coll == 'lessonid':
-            x_df.replace({'lessonid': lesson_di}, inplace=True)
+    # Create final dataframe containing sum of all question pairs for each Unit (section)
+    telecides = pd.crosstab(index=final_df['section'], columns=final_df['correct_answer'])
 
-        # Create final dataframe containing sum of all question pairs for each Unit (section)
-        x_df = pd.crosstab(index=x_df[coll], columns=x_df['correct_answer'])
+    # Create necessary column if they do not exist
+    if 'n-n' not in telecides.columns:
+        telecides['n-n'] = 0
+    if 'n-p' not in telecides.columns:
+        telecides['n-p'] = 0
+    if 'p-p' not in telecides.columns:
+        telecides['p-p'] = 0
+    if 'p-n' not in telecides.columns:
+        telecides['p-n'] = 0
 
-        # Create necessary column if they do not exist
-        if 'n-n' not in x_df.columns:
-            x_df['n-n'] = 0
-        if 'n-p' not in x_df.columns:
-            x_df['n-p'] = 0
-        if 'p-p' not in x_df.columns:
-            x_df['p-p'] = 0
-        if 'p-n' not in x_df.columns:
-            x_df['p-n'] = 0
+    # Create column 'x-n' that is sum of 'n-n' and 'p-n' columns
+    telecides['x-n'] = telecides['n-n'] + telecides['p-n']
+    telecides.drop(columns=['n-n', 'p-n'], inplace=True)
 
-        # Create column 'x-n' that is sum of 'n-n' and 'p-n' columns
-        x_df['x-n'] = x_df['n-n'] + x_df['p-n']
-        x_df.drop(columns=['n-n', 'p-n'], inplace=True)
+    # Create column sum of all pairs per Unit
+    telecides['sum'] = telecides['n-p'] + telecides['p-p'] + telecides['x-n']
 
-        # Create column sum of all pairs per Unit
-        x_df['sum'] = x_df['n-p'] + x_df['p-p'] + x_df['x-n']
+    # Sum of all question pairs
+    summa = telecides['sum'].sum()
 
-        # Number of units
-        if coll == 'section':
-            temas = len(x_df.index)
+    # Number of units
+    temas = len(telecides.index)
 
-        # Sum of all question pairs
-        summa = x_df['sum'].sum()
+    # Create final values using average probability
+    telecides['n-p'] = telecides['n-p'] / telecides['sum']
+    telecides['p-p'] = telecides['p-p'] / telecides['sum']
+    telecides['x-n'] = telecides['x-n'] / telecides['sum']
+    telecides.drop(columns=['sum'], inplace=True)
 
-        # Create final values using average probability
-        x_df['n-p'] = x_df['n-p'] / x_df['sum']
-        x_df['p-p'] = x_df['p-p'] / x_df['sum']
-        x_df['x-n'] = x_df['x-n'] / x_df['sum']
-        x_df.drop(columns=['sum'], inplace=True)
+    # Create dataframe for visualising 'Complete learning acquisition landscape'
+    df_tele = pd.DataFrame(data=[['too complicated content', 0.222, 0.111, 0.666],
+                                 ['too easy content', 0, 1, 0, ],
+                                 ['ideally matching content', 0.667, 0.333, 0]],
+                           columns=['content', 'N-P', 'P-P', 'X-N'], index=None)
+    # print(df_tele)
 
-        fig = go.Figure(data=[
-            go.Mesh3d(
-                x=df_tele['N-P'],
-                y=df_tele['P-P'],
-                z=df_tele['X-N'],
-                color='steelblue',
-                opacity=0.3,
-                hoverinfo='none',
-            ),
-            go.Scatter3d(
-                x=x_df['n-p'],
-                y=x_df['p-p'],
-                z=x_df['x-n'],
-                mode="markers",
-                text=x_df.index,
-                hovertemplate='Piemērots: %{x:.2f}<br>Viegls: %{y:.2f}<br>Nepiemērots: %{z:.2f}<extra>%{text}</extra>',
-                marker=dict(size=8, symbol="circle", color=krasa)  # color=student_df, colorscale='balance'
-            ),
-        ])
-
-        fig.update_layout(
-            #             template='seaborn',
-            scene_camera=camera,
-            scene=dict(
-                xaxis_title="Matching",
-                yaxis_title="Easy",
-                zaxis_title="Complicated",
-                xaxis=dict(
-                    nticks=6, range=[0, 1],
-                    backgroundcolor="rgb(200, 200, 230)",
-                    gridcolor="white",
-                    showbackground=True,
-                    zerolinecolor="white",
-                    showspikes=False),
-                yaxis=dict(
-                    nticks=6, range=[0, 1],
-                    backgroundcolor="rgb(230, 200,230)",
-                    gridcolor="white",
-                    showbackground=True,
-                    zerolinecolor="white",
-                    showspikes=False),
-                zaxis=dict(
-                    nticks=6, range=[0, 1],
-                    backgroundcolor="rgb(230, 230,200)",
-                    gridcolor="white",
-                    showbackground=True,
-                    zerolinecolor="white",
-                    showspikes=False),
-            ),
-            height=700,
-            margin=dict(
-                r=0, l=0,
-                b=0, t=0),
-        )
-        return fig
-
-    fig1 = final_aggregation('section', df, ab)
-    fig2 = final_aggregation('lessonid', df, ab)
-    fig3 = final_aggregation('user_id', df, ab)
-
-    return fig1, fig2, fig3, temas, number_of_students, pari, summa, value
+    fig = go.Figure(data=[
+        go.Mesh3d(
+            x=df_tele['N-P'],
+            y=df_tele['P-P'],
+            z=df_tele['X-N'],
+            color='steelblue',
+            opacity=0.3,
+            hoverinfo='skip',
+        ),
+        go.Scatter3d(
+            x=telecides['n-p'],
+            y=telecides['p-p'],
+            z=telecides['x-n'],
+            mode="markers",
+            text=telecides.index-1,
+            hovertemplate='Matching: %{x:.2f}<br>Easy: %{y:.2f}<br>Complicated: %{z:.2f}<extra>Unit: %{text}</extra>',
+            marker=dict(size=7, symbol="circle", color=telecides)  # color=telecides.index, colorscale='balance'
+        ),
+    ])
+    fig.update_layout(
+        template='plotly',
+        scene_camera=camera,
+        scene=dict(
+            xaxis_title="Matching",
+            yaxis_title="Easy",
+            zaxis_title="Complicated",
+            xaxis=dict(
+                nticks=6, range=[0, 1],
+                backgroundcolor="rgb(200, 200, 230)",
+                gridcolor="white",
+                showbackground=True,
+                zerolinecolor="white",
+                showspikes=False),
+            yaxis=dict(
+                nticks=6, range=[0, 1],
+                backgroundcolor="rgb(230, 200,230)",
+                gridcolor="white",
+                showbackground=True,
+                zerolinecolor="white",
+                showspikes=False),
+            zaxis=dict(
+                nticks=6, range=[0, 1],
+                backgroundcolor="rgb(230, 230,200)",
+                gridcolor="white",
+                showbackground=True,
+                zerolinecolor="white",
+                showspikes=False),
+        ),
+        height=600,
+        margin=dict(
+            r=0, l=0,
+            b=0, t=0),
+    )
+    return fig, temas, number_of_students, pari, summa, value
 
 
 # -------------------------------------------------------------------------------------------------------------------
